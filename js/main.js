325c9bfa1d0abce0c484d0a1b3cc83304dc24627
Ananda Rio <anandarionunez@gmail.com>
2018-10-09 16:49:19 +0200

    Search console verification

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Open the drawer when the menu icon is clicked.
+var hamburger = document.querySelector('.header_hamburger');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.header_navbar');
+
+hamburger.addEventListener('click', function (e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
